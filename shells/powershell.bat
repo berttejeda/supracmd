0d091be13d2a6ba14148fb31b8f18723e90c98b7
Root <toor@ufn.local>
2016-11-18 20:59:23 -0500

    Added path specification to powershell subscript

--- shells/powershell.bat
+++ shells/powershell.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe
+@setlocal enableextensions & C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -noexit -command $env:PATH+=';';$env:PATH+=$(cat %~dp0\cfg\default.ini).split('=')[1]
